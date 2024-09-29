64a2a991883750f32edd4d2a0f5786b3e2987bde
melisguclu <melisguclu26@gmail.com>
2024-09-29 16:26:23 +0300

    Add SettingsButton component

---
+++ src/components/SettingsButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SettingsButton = () => {
+  const handleSettingsClick = () => {
+    // Ayarlar sayfası
+  };
+
+  return (
+    <button className="settings-button" onClick={handleSettingsClick}>
+      Ayarlar
+    </button>
+  );
+};
+
+export default SettingsButton;
