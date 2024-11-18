2a7b366cf60fbbb6c7678b666042fc101cd90bc2
melisguclu <melisguclu26@gmail.com>
2024-11-18 20:31:37 +0300

    Add AI type selection in Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,13 +3,19 @@ import '../styles/Modal.css';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
-const Modal = ({ onClose, onSelect }) => {
+//we need to make some changes in order to do that, so in the modal: if the user selects "human vs ai" game , then 2 checkboxes should appear on the modal that says -random -minimax algorithm.
+
+
+const Modal = ({ onClose, onSelect , onAISelect }) => {
   const [playType, setPlayType] = useState('human-vs-ai');
+  const [aiType, setAiType] = useState('random');
+
 
   const handleSelect = (option) => {
     setPlayType(option.value);
     onSelect(option.value);
     console.log(option.value);
+    
   };
 
   const options = [
@@ -24,6 +30,8 @@ const Modal = ({ onClose, onSelect }) => {
         <span className="close" onClick={onClose}>&times;</span>
         <h2>Select Play Type</h2>
         <Dropdown options={options} onChange={handleSelect} value={defaultOption} placeholder="Select an option" />
+        <h2>Select AI Type</h2>
+        <Dropdown options={[{ value: 'random', label: 'Random' }, { value: 'minimax', label: 'Minimax' }]} onChange={(option) => setAiType(option.value)} value={aiType} placeholder="Select an option" />
       </div>
     </div>
   );
