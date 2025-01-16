e1330cda89f6d0bf783c177d8c120d9174eb85da
melisguclu <melisguclu26@gmail.com>
2025-01-17 00:03:50 +0300

    Add Prettier configuration and update ESLint settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,25 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-config-prettier'; // Prettier entegrasyonu
+import pluginPrettier from 'eslint-plugin-prettier'; // Prettier kurallarını uygulamak için
 
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,mjs,cjs,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
     },
-    settings: { react: { version: '18.3' } },
     plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      prettier: pluginPrettier, // Prettier eklentisi
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...pluginPrettier.rules, // Prettier kuralları
+      'prettier/prettier': 'error', // Prettier hatalarını ESLint hatası olarak gösterir
     },
   },
-]
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettier, // Prettier ile çakışmayı önler
+];
