c613b6d7ad029723b5a5427ce742e9e7049b830e
melisguclu <melisguclu26@gmail.com>
2025-01-17 00:48:05 +0300

    change score board to use styled components

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
 import '../styles/ScoreBoard.css';
+import styled from 'styled-components';
 
 const ScoreBoard = ({ score, playType, aiType }) => {
   return (
-    <div className="score-board" style={{ display: 'flex', justifyContent: 'space-around', gap: '20px' }}>
-      <h2>Play Type: {playType}</h2>
-      {playType === 'human-vs-ai' && <h2>AI Type: {aiType}</h2>}
-      <h2>Black: {score.B}</h2>
-      <h2>White: {score.W}</h2>
-    </div>
+    <ScoreBoardContainer>
+      <ScoreHeading>Play Type: {playType}</ScoreHeading>
+      {playType === 'human-vs-ai' && (
+        <ScoreHeading>AI Type: {aiType}</ScoreHeading>
+      )}
+      <ScoreHeading>Black: {score.B}</ScoreHeading>
+      <ScoreHeading>White: {score.W}</ScoreHeading>
+    </ScoreBoardContainer>
   );
 };
 
+const ScoreBoardContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  gap: 20px;
+  background-color: #ffffff;
+  padding: 10px 20px;
+  border-radius: 8px;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  flex-wrap: wrap;
 
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  @media (max-width: 480px) {
+    padding: 10px;
+  }
+`;
+
+const ScoreHeading = styled.h2`
+  font-size: 16px;
+  margin: 0;
+  flex: 1 1 auto;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 12px;
+  }
+`;
 
 export default ScoreBoard;
