76b83d48d37a500e19cb06a0b551061f42ffa18d
melisguclu <melisguclu26@gmail.com>
2025-01-25 22:04:30 +0300

    fix import path for UserContext in Game component

--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -18,7 +18,7 @@ import {
 } from '../utils/boardUtils';
 import { toast } from 'react-hot-toast';
 import { useContext } from 'react';
-import { UserContext } from '../../context/userContext';
+import { UserContext } from '../context/userContext';
 import { useSocket } from '../context/SocketContext';
 
 const initialBoard = () => {
